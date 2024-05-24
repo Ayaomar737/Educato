688af5b402706fc1c8c896aa04b26eb14cdb4cac
Aya Omar <113836699+Ayaomar737@users.noreply.github.com>
2024-05-24 18:42:00 +0300

    Add files via upload

---
+++ js/plugin.js
@@ -0,0 +1,16 @@
+$(function(){
+
+    'use strict';
+
+    // mobile toggle
+
+    $(".mobile-toggle").click(function(){
+        $(".sidebar").toggleClass("change");
+      });
+
+    //   datatables
+
+    $('#example').DataTable();
+    
+    
+});
