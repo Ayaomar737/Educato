662ee2d0d8309eddfc1b95afbd6957140e1da143
Aya Omar <113836699+Ayaomar737@users.noreply.github.com>
2024-05-26 00:08:49 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+
+// sidebar menu
+
+document.querySelectorAll('.sidebar-submenu').forEach(e => {
+  e.querySelector('.sidebar-menu-dropdown').onclick = (event) => {
+      event.preventDefault()
+      e.querySelector('.sidebar-menu-dropdown .dropdown-icon').classList.toggle('active')
+
+      let dropdown_content = e.querySelector('.sidebar-menu-dropdown-content')
+      let dropdown_content_lis = dropdown_content.querySelectorAll('li')
+
+      let active_height = dropdown_content_lis[0].clientHeight * dropdown_content_lis.length
+
+      dropdown_content.classList.toggle('active')
+
+      dropdown_content.style.height = dropdown_content.classList.contains('active') ? active_height + 'px' : '0'
+  }
+})
+
+
+
+// chart
+
 const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
   type: 'bar',
@@ -47,21 +70,5 @@ const myChart = new Chart(ctx, {
 });
 
 
-// sidebar menu
 
-document.querySelectorAll('.sidebar-submenu').forEach(e => {
-  e.querySelector('.sidebar-menu-dropdown').onclick = (event) => {
-      event.preventDefault()
-      e.querySelector('.sidebar-menu-dropdown .dropdown-icon').classList.toggle('active')
-
-      let dropdown_content = e.querySelector('.sidebar-menu-dropdown-content')
-      let dropdown_content_lis = dropdown_content.querySelectorAll('li')
-
-      let active_height = dropdown_content_lis[0].clientHeight * dropdown_content_lis.length
-
-      dropdown_content.classList.toggle('active')
-
-      dropdown_content.style.height = dropdown_content.classList.contains('active') ? active_height + 'px' : '0'
-  }
-})
 
--- js/plugin.js
+++ js/plugin.js
@@ -11,6 +11,17 @@ $(function(){
     //   datatables
 
     $('#example').DataTable();
+
+    // tabs 
+
+$('.tabs-list li').on('click', function (){
+
+    $(this).addClass('show').siblings().removeClass('show');
+
+    $('.content-list > div').hide();
+
+    $($(this).data('content')).fadeIn();
+});
     
     
 });
